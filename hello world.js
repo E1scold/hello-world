c5859eed1c3e1317da877eb35e76739c84bdc1cd
Damian Eis <Damian Eis>
2016-01-21 22:41:58 +0100

    hello git

---
+++ hello world.js
@@ -0,0 +1,32 @@
+if (Meteor.isClient) {
+  // counter starts at 0
+  Session.setDefault('counter', 0);
+
+  Template.hello.helpers({
+    counter: function () {
+      return Session.get('counter');
+    }
+  });
+
+  Template.hello.events({
+    'click button': function () {
+      // increment the counter when button is clicked
+      Session.set('counter', Session.get('counter') + 1);
+    }
+  });
+}
+
+if (Meteor.isServer) {
+
+  Meteor.startup(function () {
+    // code to run on server at startup
+  });
+}
+
+Trold = new Mongo.Collection('trold')
+
+Meteor.methods({
+  indsætTrold() {
+    Trold.insert({trold:true})
+  }
+})
